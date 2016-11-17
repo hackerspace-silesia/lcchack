ab1618d7126d410211bdb68796085892e87ca74b
Firemark <marpiechula@gmail.com>
2016-11-17 17:56:32 +0100

    add ranges

--- lcc_parser.py
+++ lcc_parser.py
@@ -3,15 +3,15 @@ import re
 
 
 class LccData(object):
+    re_split_start_classes = re.compile(r'([A-Z]{1,3}) *([A-Z0-9.]*)')
+    re_split_end_classes = re.compile(r'([A-Z]{1,3})? *([A-Z0-9.]*)')
+    re_split_the_rest = re.compile(r'(^[A-Z][0-9]*) *(?:R?([0-9]+))?')
 
-    re_split_classes = re.compile(r'([A-Z]{1,2}) *([A-Z0-9.]*)')
-    re_split_the_rest = re.compile(r'(^[A-Z][0-9]+) *(?:R?([0-9]+))?')
-
-    def __init__(self, main_class, sub_class, author=None, year=None):
-        self.main_class_start = main_class
-        self.main_class_end = main_class
-        self.sub_class_start = sub_class
-        self.sub_class_end = sub_class
+    def __init__(self, main_class_range, sub_class_range, author=None, year=None):
+        self.main_class_start = main_class_range[0]
+        self.main_class_end = main_class_range[1]
+        self.sub_class_start = sub_class_range[0]
+        self.sub_class_end = sub_class_range[1]
         self.author = author
         self.year = year
 
@@ -19,12 +19,23 @@ class LccData(object):
     def parse(cls, raw):
         raw = raw.upper()
         classes, the_rest = cls.get_classes_and_the_rest(raw)
-        main_class, sub_class = cls.split_classes(classes)
+        start, end = cls.split_classes(classes)
         author, year = cls.get_author_and_year(the_rest)
+        main_class_start, sub_class_start = cls.parse_range_class(start)
+
+        if end is not None:
+            if end[0] is None:
+                main_class_end = main_class_start
+            else:
+                main_class_end = cls.parse_main_class(end[0])
+            sub_class_end = cls.parse_sub_class(end[1])
+        else:
+            main_class_end = main_class_start
+            sub_class_end = sub_class_start
 
         return cls(
-            main_class=cls.parse_main_class(main_class),
-            sub_class=cls.parse_sub_class(sub_class),
+            main_class_range=(main_class_start, main_class_end),
+            sub_class_range=(sub_class_start, sub_class_end),
             author=author,
             year=year,
         )
@@ -42,10 +53,19 @@ class LccData(object):
 
     @classmethod
     def split_classes(cls, classes):
-        match = cls.re_split_classes.match(classes)
-        if match is None:
-            raise ValueError(classes)
-        return match.groups()
+        start, delimeter, end = classes.partition('-')
+        start = start.strip()
+        end = end.strip()
+        start_match = cls.re_split_start_classes.match(start)
+        if start_match is None:
+            raise ValueError(start)
+        if not delimeter:
+            return start_match.groups(), None
+
+        end_match = cls.re_split_end_classes.match(end)
+        if end_match is None:
+            raise ValueError(end)
+        return start_match.groups(), end_match.groups()
 
     @classmethod
     def get_author_and_year(cls, the_rest):
@@ -56,12 +76,21 @@ class LccData(object):
             raise ValueError(the_rest)
         return match.groups()
 
+    @classmethod
+    def parse_range_class(cls, classes):
+        main_class = cls.parse_main_class(classes[0])
+        sub_class = cls.parse_sub_class(classes[1])
+        return (main_class, sub_class)
+
     @staticmethod
     def parse_main_class(main_class):
         return int(main_class, 36)
 
     @staticmethod
     def parse_sub_class(subclass):
+        subclass = subclass.strip()
+        if not subclass:
+            return None
         left_value, delimeter, right_value = subclass.partition('.')
         value = '{}{:<05}'.format(left_value, right_value)
-        return int(value)
+        return int(value, 36)
--- tests.py
+++ tests.py
@@ -1,6 +1,10 @@
 from lcc_parser import LccData
 
 
+# Helpers
+MainClass = LccData.parse_main_class
+SubClass = LccData.parse_sub_class
+
 def test_parse_main_class_raw_compare():
     value = LccData.parse_main_class('A1')
     assert value == 10 * (36 ** 1) + 1 * (36 ** 0)
@@ -8,28 +12,23 @@ def test_parse_main_class_raw_compare():
 
 def test_parse_sub_class_raw_compare():
     value = LccData.parse_sub_class('10')
-    assert value == 1000000
+    assert value == 1 * 36 ** 6
 
 
 def test_parse_sub_class():
-    value = LccData.parse_sub_class('5')
-    assert value == int('500000', 10)
-
-
-def test_parse_subclass_with_subclass():
     value = LccData.parse_sub_class('767')
-    assert value == int('76700000', 10)
+    assert value == int('76700000', 36)
 
 
 def test_parse_subclass_with_fractional_part():
     value = LccData.parse_sub_class('767.9')
-    assert value == int('76790000', 10)
+    assert value == int('76790000', 36)
 
 
 def test_parse_without_range():
     data = LccData.parse('HQ767.9 .C675 2014')
-    classification = LccData.parse_main_class('HQ')
-    subclass = LccData.parse_sub_class('767.9')
+    classification = MainClass('HQ')
+    subclass = SubClass('767.9')
     assert data.main_class_start == classification
     assert data.main_class_end == classification
     assert data.sub_class_start == subclass
@@ -48,4 +47,36 @@ def test_parse_without_range_and_year_and_author():
     data = LccData.parse('HQ767.9')
     assert data.author is None
     assert data.year is None
+
+
+def test_parse_with_range():
+    data = LccData.parse('N5300 - 5308')
+    assert data.main_class_start == MainClass('N')
+    assert data.main_class_end == MainClass('N')
+    assert data.sub_class_start == SubClass('5300')
+    assert data.sub_class_end == SubClass('5308')
+
+
+def test_parse_with_range_with_fractional_part_on_right_side():
+    data = LccData.parse('N7101 - 7113.3')
+    assert data.main_class_start == MainClass('N')
+    assert data.main_class_end == MainClass('N')
+    assert data.sub_class_start == SubClass('7101')
+    assert data.sub_class_end == SubClass('7113.3')
+
+
+def test_parse_with_range_with_fractional_part_on_left_side():
+    data = LccData.parse('ML75.5 - 76')
+    assert data.main_class_start == MainClass('ML')
+    assert data.main_class_end == MainClass('ML')
+    assert data.sub_class_start == SubClass('75.5')
+    assert data.sub_class_end == SubClass('76')
+
+
+def test_parse_with_range_main_class():
+    data = LccData.parse('A5 - B10')
+    assert data.main_class_start == MainClass('A')
+    assert data.main_class_end == MainClass('B')
+    assert data.sub_class_start == SubClass('5')
+    assert data.sub_class_end == SubClass('10')
 
